f3e3cfd0976b9cd06bc43545aa3cb2d95db333a9
JD-IV <maxfieldegaj@gmail.com>
2018-06-07 14:50:06 -0500

    Added base controller

---
+++ Max/MaxEd.UI/Controllers/BaseController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Max.Domain.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaxEd.UI.Controllers
+{
+    public class BaseController : Controller
+    {
+        public readonly UserManager<ApplicationUser> _userManager;
+
+        public BaseController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<ApplicationUser> GetApplicationUser()
+        {
+            return await _userManager.GetUserAsync(User);
+        }
+    }
+}
