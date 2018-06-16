1ae2a413219a1ba50bb1df02cbd38bb8ad5557c2
maxfieldegaj <maxfieldegaj@gmail.com>
2018-06-16 11:40:25 -0500

    Authorization tags and testing

--- Eddy/Eddy/Eddy.UI/Controllers/EmployeeController.cs
+++ Eddy/Eddy/Eddy.UI/Controllers/EmployeeController.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Eddy.UI.Controllers
 {
+    [Authorize(Roles = "Employee")]
     public class EmployeeController : Controller
     {
         public IActionResult Index()
--- Eddy/Eddy/Eddy.UI/Controllers/ManagerController.cs
+++ Eddy/Eddy/Eddy.UI/Controllers/ManagerController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Eddy.UI.Controllers
 {
     public class ManagerController : Controller
     {
+        [Authorize(Roles = "Manager")]
         public IActionResult Index()
         {
             return View();
