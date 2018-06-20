c39c7f413147d2300c53850f6cfe49e9dba8d18e
maxfieldegaj <maxfieldegaj@gmail.com>
2018-06-20 13:47:48 -0500

    Updates to Index view for manager and employee

--- Eddy/Eddy/Eddy.Domain/Models/Shift.cs
+++ Eddy/Eddy/Eddy.Domain/Models/Shift.cs
@@ -7,7 +7,7 @@ namespace Eddy.Domain.Models
     public class Shift
     {
         public int ID { get; set; }
-        public int EmployeeID { get; set; }
+        public Person AssignedTo { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
         public bool TradePending { get; set; }
--- Eddy/Eddy/Eddy.UI/ViewModels/EmployeeNavBarViewModel.cs
+++ Eddy/Eddy/Eddy.UI/ViewModels/EmployeeNavBarViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Eddy.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,6 @@ namespace Eddy.UI.ViewModels
 {
     public class EmployeeNavBarViewModel
     {
+        public ApplicationUser ApplicationUser { get; set; }
     }
 }
